a52f6ef00410732ba1f94aaa0a134e8ed13927f3
Auto Commit <autocommit@example.com>
2026-05-17 18:58:40 -0700

    Auto commit (2026-05-17 18:58:40)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,7 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
             pickFirsts += "META-INF/versions/9/OSGI-INF/MANIFEST.MF"
             pickFirsts += "META-INF/versions/11/OSGI-INF/MANIFEST.MF"
+            pickFirsts += "META-INF/versions/15/OSGI-INF/MANIFEST.MF"
         }
     }
 }
