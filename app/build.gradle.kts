d514969ece875d94b1a9a6ff14e03ab35a0b767a
Ghost Developer <139102805+Gh0stDeveloper@users.noreply.github.com>
2026-07-31 09:41:58 -0600

    Bump application to 1.0.43

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.ghostnexora.vpn"
         minSdk = 26
         targetSdk = 35
-        versionCode = 42
-        versionName = "1.0.42"
+        versionCode = 43
+        versionName = "1.0.43"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
